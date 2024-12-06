2d03f065622b075cc8e03000ad556cf6ee3877f3
Andrew Cobb <andrew.cobb@gmail.com>
2024-12-06 01:09:34 -0700

    day 06, slow

--- 2024/Program.fs
+++ 2024/Program.fs
@@ -1,6 +1,7 @@
 ﻿module Program
 
 open System
+open System.Threading
 open Day
 open FSharpx
 open FSharpx.Option
@@ -9,13 +10,14 @@ open FSharpx.IO
 open FSharpx.Collections
 
 let allDays =
-    List.monoid {
-        [ Day01.day01 ]
-        [ Day02.day02 ]
-        [ Day03.day03 ]
-        [ Day04.day04 ]
-        [ Day05.day05 ]
-    }
+    [
+        Day01.day01
+        Day02.day02
+        Day03.day03
+        Day04.day04
+        Day05.day05
+        Day06.day06
+    ]
 
 let days =
     let dayArgs = Environment.GetCommandLineArgs () |> Array.skip 1 |> List.ofArray
@@ -41,37 +43,42 @@ let days =
             printfn $"Unknown days requested: %A{bad}"
             exit 1
 
-flip List.map days
-<| fun day ->
-    let dayPath = $"day-%02d{day.dayNumber}"
-    printfn $"=== %s{dayPath} ==="
+let doTheWork () =
+    flip List.map days
+    <| fun day ->
+        let dayPath = $"day-%02d{day.dayNumber}"
+        printfn $"=== %s{dayPath} ==="
 
-    timer
-    <| fun () ->
-        flip List.map day.inputs
-        <| fun (inputFile, expected) ->
-            let lines = readFile $"%s{dayPath}/%s{inputFile}" |> List.ofSeq
-            printf $"%s{inputFile}: "
-            let result0 = day.solvePart0 lines
-            printf $"%A{result0} "
-            let result1 = day.solvePart1 lines
-            printf $"%A{result1} "
+        timer
+        <| fun () ->
+            flip List.map day.inputs
+            <| fun (inputFile, expected) ->
+                let lines = readFile $"%s{dayPath}/%s{inputFile}" |> List.ofSeq
+                printf $"%s{inputFile}: "
+                let result0 = day.solvePart0 lines
+                printf $"%A{result0} "
+                let result1 = day.solvePart1 lines
+                printf $"%A{result1} "
 
-            match expected with
-            | Some expectation ->
-                if expectation = (result0, result1) then
-                    printfn " \u2705 good"
+                match expected with
+                | Some expectation ->
+                    if expectation = (result0, result1) then
+                        printfn " \u2705 good"
+                        true
+                    else
+                        printfn " \u274c bad"
+                        false
+                | None ->
+                    printfn ""
                     true
-                else
-                    printfn " \u274c bad"
-                    false
-            | None ->
-                printfn ""
-                true
-        |> List.forall id
-|> (fun successes ->
-    if not (List.forall id successes) then
-        (printfn ""
-         printfn "\u274c Exiting due to earlier errors"
-         exit 1)
-)
+            |> List.forall id
+    |> (fun successes ->
+        if not (List.forall id successes) then
+            (printfn ""
+             printfn "\u274c Exiting due to earlier errors"
+             exit 1)
+    )
+
+let workerThread = Thread (doTheWork, Int32.MaxValue / 32 * 32)
+workerThread.Start ()
+workerThread.Join ()
---
+++ 2024/day-06/day-06.fs
@@ -0,0 +1,93 @@
+﻿module Day06
+
+open FSharpx.Collections
+open FSharpx.Text
+open FSharpx
+open Utils
+
+type Dir =
+    | N
+    | E
+    | S
+    | W
+
+let step (x, y) =
+    function
+    | N -> (x, y - 1)
+    | E -> (x + 1, y)
+    | S -> (x, y + 1)
+    | W -> (x - 1, y)
+
+let rotate =
+    function
+    | N -> E
+    | E -> S
+    | S -> W
+    | W -> N
+
+let parse (input : string list) : char array array =
+    input
+    |> List.filter (not << Strings.isNullOrEmpty)
+    |> List.map Array.ofSeq
+    |> Array.ofList
+
+let find2 (arr : 'a array array) (elem : 'a) : (int * int) seq =
+    seq {
+        for y in 0 .. Array.length arr - 1 do
+            for x in 0 .. Array.length arr[y] - 1 do
+                if arr[y][x] = elem then
+                    yield (x, y)
+    }
+
+let solvePart0 (input : string list) : int =
+    let grid = parse input
+    let start = find2 grid '^' |> Seq.head
+    let height, width = Array.length grid, Array.length grid[0]
+
+    let rec travel (x, y) (seen : Set<int * int>) (dir : Dir) =
+        let seen = Set.add (x, y) seen
+        let x', y' = step (x, y) dir
+
+        if x' < 0 || width <= x' || y' < 0 || height <= y' then
+            seen
+        else if grid[y'][x'] = '#' then
+            travel (x, y) seen (rotate dir)
+        else
+            travel (x', y') seen dir
+
+    travel start Set.empty N |> Set.count
+
+let visited (seen : Set<int * int * Dir>) (x, y) =
+    Seq.exists (fun d -> Set.contains (x, y, d) seen) [ N ; E ; S ; W ]
+
+let solvePart1 (input : string list) : int =
+    let grid = parse input
+    let start = find2 grid '^' |> Seq.head
+    let height, width = Array.length grid, Array.length grid[0]
+
+    let rec travel (x, y) (seen : Set<int * int * Dir>) (deviated : (int * int) option) (dir : Dir) =
+
+        if Set.contains (x, y, dir) seen then
+            Set.ofList (Option.toList deviated)
+        else
+
+        let seen = Set.add (x, y, dir) seen
+        let x', y' = step (x, y) dir
+
+        if x' < 0 || width <= x' || y' < 0 || height <= y' then
+            Set.empty
+        else if grid[y'][x'] = '#' || Some (x', y') = deviated then
+            travel (x, y) seen deviated (rotate dir)
+        else
+            Set.union
+                (match deviated with
+                 | None when not <| visited seen (x', y') -> travel (x, y) seen (Some (x', y')) (rotate dir)
+                 | _ -> Set.empty)
+                (travel (x', y') seen deviated dir)
+
+    travel start Set.empty None N |> Set.count
+
+let day06 =
+    Day.day 06 solvePart0 solvePart1
+    |> Day.addInput "input-ex0.txt" (Some (41, 6))
+    |> Day.addInput "input-real0.txt" (Some (4967, 1789))
