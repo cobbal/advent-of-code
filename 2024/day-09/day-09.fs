3c0dc70f55bc18f06908d8cbbf2c1de05f95f216
Andrew Cobb <andrew.cobb@gmail.com>
2024-12-09 14:52:00 -0700

    09: functionalize part 0 more

--- 2024/day-09/day-09.fs
+++ 2024/day-09/day-09.fs
@@ -1,31 +1,22 @@
 ﻿module Day09
 
 open FSharpx.Collections
-open FSharpx.Text
 open FSharpx
-open FSharpx.Text.Regex
 open Utils
 
 let parse (input : string list) : int list =
     input |> List.head |> List.ofSeq |> List.map (fun c -> int $"{c}")
 
-let rec checksum (buffer : int array) pos acc =
-    if pos < buffer.Length then
-        checksum buffer (pos + 1) (acc + int64 pos * max 0L (int64 buffer[pos]))
-    else
-        acc
-
 let solvePart0 (input : string list) : int64 =
     let lens = parse input @ [ 0 ]
     let totalLen = List.sum lens
-    let buffer = Array.create totalLen -1
 
-    let rec loadBuffer pos id =
+    let rec loadBuffer acc id =
         function
-        | x :: y :: xs ->
-            Array.fill buffer pos x id
-            loadBuffer (pos + x + y) (id + 1) xs
-        | _ -> ()
+        | x :: y :: xs -> loadBuffer (Array.create y -1 :: Array.create x id :: acc) (id + 1) xs
+        | _ -> List.rev acc |> Array.concat
+
+    let buffer = loadBuffer [] 0 lens
 
     let rec compact acc fwd rev =
         if rev < fwd then
@@ -35,9 +26,8 @@ let solvePart0 (input : string list) : int64 =
         else if buffer[rev] = -1 then
             compact acc fwd (rev - 1)
         else
-            compact (acc + int64 (fwd * buffer[rev]))(fwd + 1) (rev - 1)
+            compact (acc + int64 (fwd * buffer[rev])) (fwd + 1) (rev - 1)
 
-    loadBuffer 0 0 lens
     compact 0L 0 (totalLen - 1)
 
 [<Struct>]
@@ -45,15 +35,11 @@ type File = { Id : int ; Pos : int ; Len : int }
 
 let solvePart1 (input : string list) : int64 =
     let lens = parse input @ [ 0 ]
-    let totalLen = List.sum lens
-    let buffer = Array.create totalLen -1
 
     let rec loadDisk (i : int) (pos : int) (acc : File list) : int list -> File array =
         function
         | x :: y :: xs -> loadDisk (i + 1) (pos + x + y) ({ Id = i ; Pos = pos ; Len = x } :: acc) xs
         | _ -> List.rev acc |> Array.ofList
-
-    let disk = loadDisk 0 0 [] lens
 
     let rec defrag1 (i : int) (pos : int) (file : File) (disk : File array) : File array =
         let nextFile = disk[int i]
@@ -67,22 +53,18 @@ let solvePart1 (input : string list) : int64 =
 
     let rec dedup seen =
         function
-        | file :: rest when not (Map.containsKey file.Id seen) ->
-            dedup (Map.add file.Id file seen) rest
+        | file :: rest when not (Map.containsKey file.Id seen) -> dedup (Map.add file.Id file seen) rest
         | _ :: rest -> dedup seen rest
         | [] -> seen
+
+    let disk = loadDisk 0 0 [] lens
 
     let defragged =
         Array.foldBack (defrag1 0 0) disk disk |> List.ofArray |> dedup Map.empty
 
     defragged
     |> Map.values
-    |> Seq.iter (fun file ->
-        for i in file.Pos .. file.Pos + int file.Len - 1 do
-            buffer[int i] <- int file.Id
-    )
-
-    checksum buffer 0 0L
+    |> Seq.fold (fun acc file -> acc + int64 file.Id * int64 file.Len * int64 (2 * file.Pos + file.Len - 1) / 2L) 0L
 
 let day09 =
     Day.day 09 solvePart0 solvePart1
