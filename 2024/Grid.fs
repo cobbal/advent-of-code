386b101f2bac72b65a4d38a885546e38474b25e8
Andrew Cobb <andrew.cobb@gmail.com>
2024-12-12 17:31:41 -0700

    refactor grid into its own file

---
+++ 2024/Grid.fs
@@ -0,0 +1,65 @@
+﻿module Grid
+
+open System.Diagnostics
+
+[<Struct>]
+type XY =
+    val X : int
+    val Y : int
+    new(x, y) = { X = x ; Y = y }
+
+    [<DebuggerBrowsable(DebuggerBrowsableState.Never)>]
+    member this.N = XY (this.X, this.Y - 1)
+    [<DebuggerBrowsable(DebuggerBrowsableState.Never)>]
+    member this.E = XY (this.X + 1, this.Y)
+    [<DebuggerBrowsable(DebuggerBrowsableState.Never)>]
+    member this.S = XY (this.X, this.Y + 1)
+    [<DebuggerBrowsable(DebuggerBrowsableState.Never)>]
+    member this.W = XY (this.X - 1, this.Y)
+
+    override this.ToString() = $"%d{this.X},%d{this.Y}"
+
+type Grid(grid : byte array, width : int, height : int) =
+    member this.Grid = grid
+    member this.Width = width
+    member this.Height = height
+
+    member this.ContainsIndex (pos : XY) =
+        0 <= pos.X && pos.X < this.Width && 0 <= pos.Y && pos.Y < this.Height
+
+    member this.Item
+        with get pos : char =
+            if this.ContainsIndex pos then
+                char this.Grid[pos.Y * width + pos.X]
+            else
+                char 0
+        and set pos (value : char) =
+            assert this.ContainsIndex pos
+            this.Grid[pos.Y * width + pos.X] <- byte value
+
+    new(input : string seq)
+        =
+        let grid =
+            Seq.map (fun (s : string) -> System.Text.Encoding.UTF8.GetBytes s) input
+            |> Array.ofSeq
+
+        let width = Array.length grid[0]
+        let height = Array.length grid
+
+        Grid (Array.concat grid, width, height)
+
+    member this.Copy () = Grid (Array.copy grid, width, height)
+
+    member this.Indices () =
+        seq {
+            for y in 0 .. height - 1 do
+                for x in 0 .. width - 1 do
+                    yield XY (x, y)
+        }
+
+    member this.FindIndicesOf (target : char) =
+        seq {
+            for pos in this.Indices () do
+                if this[pos] = target then
+                    yield pos
+        }
--- 2024/day-06/day-06.fs
+++ 2024/day-06/day-06.fs
@@ -4,6 +4,7 @@ open System
 open FSharpx.Collections
 open FSharpx.Text
 open Utils
+open Grid
 
 type Dir =
     | N = 8
@@ -14,16 +15,13 @@ type Dir =
 
 type DirSet = int
 
-[<Struct>]
-type XY = { x : int ; y : int }
-
 let moveInDirection : XY -> Dir -> XY =
     fun pos ->
         function
-        | Dir.N -> { x = pos.x ; y = pos.y - 1 }
-        | Dir.E -> { x = pos.x + 1 ; y = pos.y }
-        | Dir.S -> { x = pos.x ; y = pos.y + 1 }
-        | Dir.W -> { x = pos.x - 1 ; y = pos.y }
+        | Dir.N -> pos.N
+        | Dir.E -> pos.E
+        | Dir.S -> pos.S
+        | Dir.W -> pos.W
         | _ -> ArgumentOutOfRangeException () |> raise
 
 let rotate =
@@ -34,111 +32,78 @@ let rotate =
     | Dir.W -> Dir.N
     | _ -> ArgumentOutOfRangeException () |> raise
 
-let parse (input : string list) : char array array =
-    input
-    |> List.filter (not << Strings.isNullOrEmpty)
-    |> List.map Array.ofSeq
-    |> Array.ofList
-
 [<Struct>]
 type Step = { pos : XY ; dir : Dir }
 
 module Step =
-    let Escaped =
-        {
-            pos = { x = 0 ; y = 0 }
-            dir = Dir.Escaped
-        }
+    let Escaped = { pos = XY (0, 0) ; dir = Dir.Escaped }
 
     let isEscaped step = step.dir = Dir.Escaped
 
-type Grid(grid : char array, width : int, height : int, start : XY) =
-    member this.grid = grid
-    member this.width = width
-    member this.height = height
-    member this.start = start
+let travel1 (grid : Grid) pos dir =
+    let pos' = moveInDirection pos dir
 
-    member this.Item
-        with get pos = this.grid[pos.y * width + pos.x]
-        and set pos value = this.grid[pos.y * width + pos.x] <- value
+    if not (grid.ContainsIndex pos') then
+        Step.Escaped
+    else if grid[pos'] = '#' then
+        { pos = pos ; dir = rotate dir }
+    else
+        { pos = pos' ; dir = dir }
 
-    new(grid : char array array)
-        =
-        let width = Array.length grid[0]
-        let height = Array.length grid
+let trackedTravel1 (grid : Grid) (seen : Map<XY, DirSet>) pos dir =
+    let seen = Map.change pos (fun v -> Some (defaultArg v 0 ||| int dir)) seen
+    struct (seen, travel1 grid pos dir)
 
-        let start =
-            seq {
-                for y in 0 .. height - 1 do
-                    for x in 0 .. width - 1 do
-                        if grid[y][x] = '^' then
-                            yield { x = x ; y = y }
-            }
+let rec travel grid seen (pos : XY) dir : Set<XY> option =
+    match trackedTravel1 grid seen pos dir with
+    | seen', step when Step.isEscaped step -> Some (Set.ofSeq seen'.Keys)
+    | seen', step -> travel grid seen' step.pos step.dir
 
-        Grid (Array.concat grid, width, height, Seq.head start)
+let loops grid : XY -> Dir -> bool =
+    let stepOnce s =
+        if Step.isEscaped s then s else travel1 grid s.pos s.dir
 
-    member this.travel1 pos dir =
-        let pos' = moveInDirection pos dir
+    let rec loop tortoise hare =
+        let tortoise' = stepOnce tortoise
+        let hare' = stepOnce (stepOnce hare)
 
-        if pos'.x < 0 || width <= pos'.x || pos'.y < 0 || height <= pos'.y then
-            Step.Escaped
-        else if this[pos'] = '#' then
-            { pos = pos ; dir = rotate dir }
-        else
-            { pos = pos' ; dir = dir }
+        if Step.isEscaped hare' then false
+        else if tortoise' = hare' then true
+        else loop tortoise' hare'
 
-    member this.trackedTravel1 (seen : Map<XY, DirSet>) pos dir =
-        let seen = Map.change pos (fun v -> Some (defaultArg v 0 ||| int dir)) seen
-        struct (seen, this.travel1 pos dir)
+    fun pos dir ->
+        let start = { pos = pos ; dir = dir }
+        loop start start
 
-    member this.travel seen (pos : XY) dir : Set<XY> option =
-        match this.trackedTravel1 seen pos dir with
-        | seen', step when Step.isEscaped step -> Some (Set.ofSeq seen'.Keys)
-        | seen', step -> this.travel seen' step.pos step.dir
-
-    member this.loops : XY -> Dir -> bool =
-        let stepOnce s =
-            if Step.isEscaped s then s else this.travel1 s.pos s.dir
-
-        let rec loop tortoise hare =
-            let tortoise' = stepOnce tortoise
-            let hare' = stepOnce (stepOnce hare)
-
-            if Step.isEscaped hare' then false
-            else if tortoise' = hare' then true
-            else loop tortoise' hare'
-
-        fun pos dir ->
-            let start = { pos = pos ; dir = dir }
-            loop start start
-
-    member this.withObstacle pos =
-        let newGrid = Grid (Array.copy this.grid, this.width, this.height, this.start)
-        newGrid[pos] <- '#'
-        newGrid
+let withObstacle (grid : Grid) pos =
+    let newGrid = grid.Copy ()
+    newGrid[pos] <- '#'
+    newGrid
 
 let solvePart0 (input : string list) : int64 =
-    let grid = parse input |> Grid
-    grid.travel Map.empty grid.start Dir.N |> Option.get |> Set.count |> int64
+    let grid = Grid input
+    let start = Seq.head (grid.FindIndicesOf '^')
+    travel grid Map.empty start Dir.N |> Option.get |> Set.count |> int64
 
 let solvePart1 (input : string list) : int64 =
-    let grid = parse input |> Grid
+    let grid = Grid input
+    let start = Seq.head (grid.FindIndicesOf '^')
 
     let rec travel (seen : Map<XY, DirSet>) (acc : Set<XY>) pos (dir : Dir) =
-        match grid.trackedTravel1 seen pos dir with
+        match trackedTravel1 grid seen pos dir with
         | _, step when Step.isEscaped step -> acc
         | seen', step ->
             let shouldBlock = pos <> step.pos && Map.findOrDefault step.pos 0 seen = 0
 
             let acc' =
-                if shouldBlock && (grid.withObstacle step.pos).loops pos dir then
+                if shouldBlock && loops (withObstacle grid step.pos) pos dir then
                     Set.add step.pos acc
                 else
                     acc
 
             travel seen' acc' step.pos step.dir
 
-    travel Map.empty Set.empty grid.start Dir.N |> Set.count |> int64
+    travel Map.empty Set.empty start Dir.N |> Set.count |> int64
 
 let day06 =
     Day.day 06 solvePart0 solvePart1
