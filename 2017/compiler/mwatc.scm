e3283dfff667977d7523708b85f2a7da5cf72012
Andrew Cobb <andrew.cobb@gmail.com>
2025-12-04 21:52:14 -0700

    more features for the compiler

--- 2017/compiler/mwatc.scm
+++ 2017/compiler/mwatc.scm
@@ -3,9 +3,11 @@
   (ice-9 match)
   (ice-9 pretty-print)
   (ice-9 receive)
+  (rnrs bytevectors)
   (srfi srfi-1)
   (srfi srfi-9 gnu)
-  (srfi srfi-26))
+  (srfi srfi-26)
+  (srfi srfi-111))
 
 (define (displayln . args) (apply display args) (newline))
 (define (sym-coerce sym-or-str)
@@ -26,10 +28,35 @@
         (reverse acc)
         (loop (cons form acc))))))
 
+(define-immutable-record-type static-allocator
+  (make-static-allocator next-ptr strings)
+  static-allocator?
+  (next-ptr static-allocator-next-ptr set-static-allocator-next-ptr)
+  (strings static-allocator-strings set-static-allocator-strings))
+
 (define-immutable-record-type env
-  (make-env namespace)
+  (make-env namespace i-width static-allocator)
   env?
-  (namespace env-namespace set-env-namespace))
+  (namespace env-namespace set-env-namespace)
+  (i-width env-i-width set-env-i-width)
+  (static-allocator env-static-allocator))
+
+(define-immutable-record-type comment-record
+  (comment contents)
+  comment?
+  (contents comment-contents))
+(set-record-type-printer! comment-record
+  (lambda (record port)
+    (format port "(; ~a ;)" (comment-contents record))))
+
+(define (static-alloc env string)
+  (define alloc-box (env-static-allocator env))
+  (define result (static-allocator-next-ptr (unbox alloc-box)))
+  (set-box! alloc-box
+    (make-static-allocator
+      (+ result (string-utf8-length string) 1)
+      (cons `(data (i32.const ,result) ,string) (static-allocator-strings (unbox alloc-box)))))
+  result)
 
 (define (env-push-namespace env name)
   (set-env-namespace env (string-append (env-namespace env) (symbol->string name) ".")))
@@ -57,44 +84,55 @@
         `((func ,(lookup env name)
             ,@(map (cut cons 'param <>) params)
             ,@(map (cut cons 'result <>) results)
-            ,@(map (process/instr 'i32) forms))))]
+            ,@(map (process/instr env) forms))))]
     [form (error "unrecognized form:" form)]))
 
-(define (process/instr* i-width)
+(define (process/instr* env)
   (match-lambda
-    [`(i64 . ,forms) (concatenate (map (process/instr* 'i64) forms))]
-    [`(i32 . ,forms) (concatenate (map (process/instr* 'i32) forms))]
-    [form (list ((process/instr i-width) form))]))
+    [`(i32 . ,forms) (concatenate (map (process/instr* (set-env-i-width env 'i32)) forms))]
+    [`(i64 . ,forms) (concatenate (map (process/instr* (set-env-i-width env 'i64)) forms))]
+    [form (list ((process/instr env) form))]))
 
-(define (process/instr i-width)
+(define (process/instr env)
   (define (arith suffix . forms)
-    (cons (sym-append i-width "." suffix) forms))
-  (define (recur* f) ((process/instr* i-width) f))
-  (define (recur f) ((process/instr i-width) f))
+    (cons (sym-append (env-i-width env) "." suffix) forms))
+  (define (recur* f) ((process/instr* env) f))
+  (define (recur f) ((process/instr env) f))
   (match-lambda
-    [`(i64 ,form) ((process/instr 'i64) form)]
-    [`(i32 ,form) ((process/instr 'i32) form)]
+    [`(i32 ,form) ((process/instr (set-env-i-width env 'i32)) form)]
+    [`(i64 ,form) ((process/instr (set-env-i-width env 'i64)) form)]
     [(? integer? i) (arith 'const i)]
     [`(+ ,form0 . ,forms)
       (fold (lambda (x y) (arith 'add y x))
         (recur form0)
         (map recur forms))]
+    [`(call ,fn . ,args)
+      `(call ,(lookup env fn) . ,(map recur args))]
+    [(? string? s)
+      `(i32.const ,(static-alloc env s) ,(comment (format #f "~s" s)))]
     [form `(UNRECOGNIZED ,form)]
     [form (error "unrecognized form:" form)]))
 
-(define (process* forms)
-  (concatenate (map (process/module (make-env "")) forms)))
+(define (process* env forms)
+  (concatenate (map (process/module env) forms)))
 
-(define (hoist-imports forms)
+(define (hoist-imports forms static-allocator)
+  (define datae (reverse (static-allocator-strings static-allocator)))
+  (define memory-size (static-allocator-next-ptr static-allocator))
+  (define memory `(memory (export "memory") ,(ceiling-quotient memory-size #x4000)))
   (receive (imports rest)
     (partition (lambda (x) (eq? (car-safe x) 'import)) forms)
-    (append imports rest)))
+    (append imports datae (list memory) rest)))
 
 (define (process-inputs paths)
   (define out-wasm "build/2017.wat")
   (format #t "Compiling ~a to ~a...\n" paths out-wasm)
-  (define modules (map (lambda (path) (process* (call-with-input-file path read*))) paths))
+  (define env (make-env "" 'i32 (box (make-static-allocator 1024 '()))))
+  (define modules (map (lambda (path) (process* env (call-with-input-file path read*))) paths))
   (call-with-output-file out-wasm
-    (cut pretty-print `(module ,@(hoist-imports (concatenate modules))) <>)))
+    (cut pretty-print
+      `(module
+         ,@(hoist-imports (concatenate modules) (unbox (env-static-allocator env))))
+      <>)))
 
 (process-inputs (cdr (program-arguments)))
